21791e7b21cdd8da17c4714590d6c598139fe066
ronaldogdm <ronaldogarciadem@gmail.com>
2017-10-05 12:07:03 -0400

    Add examples for lec13

---
+++ src/lec13-functions-as-parameters/00-reduce-app.ts
@@ -0,0 +1,46 @@
+import "introcs";
+
+function main(): void {
+
+    let numbers: number[] = [1, 2, 3, 4];
+    // TODO: call reduce method on numbers and print results
+
+    let strings: string[] = ["one", "two", "three"];
+    // TODO: call reduce method on strings and print results
+    
+}
+
+// The following functions are all Reducers
+interface Reducer<T, U> {
+    (memo: U, element: T): U;
+}
+
+function sum(memo: number, n: number): number {
+    return memo + n;
+}
+
+function min(memo: number, n: number): number {
+    if (memo < n) {
+        return memo;
+    } else {
+        return n;
+    }
+}
+
+function append(memo: string, s: string): string {
+    return memo + s;
+}
+
+function commaSeparate(memo: string, s: string): string {
+    if (memo === "") {
+        return s;
+    } else {
+        return memo + ", " + s;
+    }
+}
+
+function sumLengths(memo: number, s: string): number {
+    return memo + s.length;
+}
+
+main();
---
+++ src/lec13-functions-as-parameters/02-reduce-implementation-app.ts
@@ -0,0 +1,32 @@
+import "introcs";
+
+interface Reducer<T, U> {
+    (memo: U, element: T): U;
+}
+
+function main(): void {
+    let strings: string[] = ["one", "two", "three"];
+    print(reduce(strings, sumLengths, 0));
+
+    // TODO #2:
+    // print(reduce(strings, commaSeparate, ""));
+}
+
+function reduce(a: string[], reducer: Reducer<string, number>, memo: number): number {
+    // TODO #1 - Implement reduce using a for loop
+    return memo;
+}
+
+function sumLengths(memo: number, s: string): number {
+    return memo + s.length;
+}
+
+function commaSeparate(memo: string, s: string): string {
+    if (memo === "") {
+        return s;
+    } else {
+        return memo + ", " + s;
+    }
+}
+
+main();
