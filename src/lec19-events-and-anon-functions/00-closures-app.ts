c15e3f185dcef1d1c1d314387a3193d74b80273b
ronaldogdm <ronaldogarciadem@gmail.com>
2017-10-31 11:41:43 -0400

    Add closure example

---
+++ src/lec19-events-and-anon-functions/00-closures-app.ts
@@ -0,0 +1,29 @@
+import "introcs";
+
+interface VoidFunction {
+    (): void;
+}
+
+function main(): void {
+    
+    // The f function is declared in main's block
+    let f: VoidFunction;
+
+    // Notice: We are establishing a new block!
+    {
+        // The variable a is defined inside the new block.
+        let a: number = 110;
+
+        // f is assigned an anonymous function. It is a closure
+        // because it refers to variable a, defined outside its
+        // own body block.
+        f = function(): void {
+            print(a);
+        };
+    }
+
+    // TODO: Call f and _try_ printing a
+
+}
+
+main();
