9fd16c291e046963b8155493149f7cd9ebf0c634
ronaldogdm <ronaldogarciadem@gmail.com>
2017-11-20 10:58:58 -0500

    Update basic example for 2d

--- src/lec25-2d-arrays/2d-arrays-app.ts
+++ src/lec25-2d-arrays/2d-arrays-app.ts
@@ -7,3 +7,13 @@ import { array2d } from "./2d-arrays-helpers";
 // Initialize using Literals
 
 // Initialize using array2d helper function
+
+// Assigning to an element
+
+// Read the # of rows
+
+// Read the # of cols
+
+// Read from an element
+
+// Read from a top-level element
