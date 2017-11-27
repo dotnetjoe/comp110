c5642aac2f799f9af635c9a266bef4716053f9f4
ronaldogdm <ronaldogarciadem@gmail.com>
2017-11-27 00:34:53 -0500

    Closer to release.

--- src/ps06-compstagram/compstagram-script.ts
+++ src/ps06-compstagram/compstagram-script.ts
@@ -2,19 +2,27 @@ import { View } from "./support/View";
 import { Model } from "./support/Model";
 import { Controller } from "./support/Controller";
 
-// TODO #1: Import your Filter subclasses
+import {
+    InvertFilter,
+    BorderFilter,
+    BrightnessFilter,
+    ColorizeFilter,
+    ContrastFilter,
+    SaturationFilter
+} from "./compstagram-filters";
 
 function main(): void {
 
     let model: Model = new Model();
 
-    // TODO #2: After you write a Filter subclass and import it,
-    // add the class to the array below for it to show up in the
-    // user interface. If you make the Filter subclass you are
-    // currently working on the first element in the array, it
-    // will be the default filter shown in the filter selector. 
+    // TODO: As you work begin work on each filter, comment it out.
     model.filterClasses = [
-        
+        // SaturationFilter,
+        // ContrastFilter,
+        // ColorizeFilter,
+        // BrightnessFilter,
+        // BorderFilter,
+        InvertFilter
     ];
 
     let view: View = new View(model);
